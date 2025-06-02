ce1e124fd14ae2e17d3d86230bd09ae5dcd8481d
Александр С. Ковалев <kovalev@ptc.local>
2025-06-02 11:33:55 +0500

    2/6/25

--- formuls.py
+++ formuls.py
@@ -1,6 +1,6 @@
 import openpyxl as op
 from datetime import datetime
-
+from pprint import pprint
 
 def time(func):
     """
@@ -44,12 +44,11 @@ def round_val(value:int | float) -> int | float:
             return int(val)
         return int(value)
 
-def get_kokil(D:int | float, L=0, pos=0):
+def get_kokil(D:int | float) -> list:
     """
-    Принимает на вход диаметр_кокиля (опционально: длина_кокиля, посадка _на_цб).\n
+    Принимает на вход диаметр_кокиля.\n
     \tВозвращает:\n
-    Если указан только диаметр - список кокилей указанного диаметра (кортежи).\n
-    Остальное уточняюшее. Длина нужна всегда, посадка - оч. редко.
+    Список кокилей указанного диаметра (кортежи).\n
     """
     
     file = '//ad/технологи/Перечень центробежных изложниц.xlsx'
@@ -57,22 +56,12 @@ def get_kokil(D:int | float, L=0, pos=0):
     sheet = book['Изложницы без бурта']
     
     lst1 = []
-    lst2 = []
     for row in sheet.iter_rows(min_row=3, min_col=2, max_col=8, max_row=1159, values_only=True):
         if row[0] == D:
-            lst1.append(row)
-    if L > 0 and pos > 0:
-        for item in lst1:
-            if item[1] == L and item[3] == pos:
-                lst2.append(item)
-        return(lst2)
-    elif L:
-        for item in lst1:
-            if item[1] == L:
-                lst2.append(item)
-        return(lst2)
-    else:
-        return(list(set(lst1)))
+            if row not in lst1:
+                lst1.append(row)
+    
+    return(sorted(lst1))
 
 def massa_vtulki(d1:float | int, d2:float | int, len:float | int, ro:float,) -> float | int:
     """
@@ -119,22 +108,37 @@ def massa_planki(a:float | int, b:float | int, c:float | int, ro:float) -> float
     """
     return a*b*c*ro/1000000
 
-def massa_otlivki(d1:float | int, len1:float | int, len2:float | int, ro:float, d2=0, revers=False) -> float | int:
+def massa_bolvana_cb(d1:float | int, len1:float | int, len2:float | int, ro:float, revers=False) -> float | int:
     """
-    Вычисляет массу [отливки | болвана] при заливке в цб кокиль.\n
-    d1 - Диаметр кокиля. d2 - Диаметр отливки внутренний.\n
-    len1 - Длина кокиля. len2 - Длина отливки.\n
-    reverse - Направление заливки [от меньшего диаметра к большему (False) | обратное (True)].\n
-    ro - Плотность металла.
+    Вычисляет массу болвана при заливке в цб кокиль.\n
+    d1 - Диаметр кокиля.\n
+    len1 - Длина кокиля.\n
+    len2 - Длина отливки.\n
+    ro - Плотность металла.\n
+    reverse - Направление заливки [от большего диаметра к меньшему (False) default | обратное - как в стакан (True)].
     """
+
     diam_max_kok= len1/30+d1 # Диаметр у заливочной крышки
     if revers:
+        diam_max= len2/30+d1 # Диаметр у заливочной крышки
+        diam_avg = (d1 + diam_max)/2
+    else:
         diam_min = (len1-len2)/30+d1
         diam_avg = (diam_max_kok + diam_min)/2
-    else:
-        diam_min = (len1 - len2)/30+d1
-        diam_avg = (d1 + diam_min)/2
-    return ((diam_avg*0.99)**2-d2**2)*3.1415926*(len2*0.985)*ro/4_000_000
+    return (diam_avg*0.985)**2*3.1415926*(len2*0.985)*ro/4_000_000
+    
+def get_diam2_kok(d1:int, len:int) -> tuple:
+    """
+    Принимает на вход диаметр кокиля и его длину.\n
+    Возвращает: кортэж с всей инфой.
+    """
+    file = '//ad/технологи/Заказы/ЦБ ОСНАСТКА+ИЗЛОЖНИЦЫ.xlsx'
+    book = op.load_workbook(file)
+    ws = book['Изложницы']
+
+    for row in ws.iter_rows(min_row=2, min_col=1, max_col=9, max_row=45, values_only=True):
+        if d1 == row[0] and len == row[2]:
+            return row
 
 
-print(massa_otlivki(180,380,334,7.5,140,revers=False))
+pprint(round_val(105.5))
